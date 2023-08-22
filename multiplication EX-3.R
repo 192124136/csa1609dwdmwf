eca66c4d58d779f42c7c3155347ef6d1bff67c58
Meenaz.G <112608933+192124136@users.noreply.github.com>
2023-08-22 22:52:33 +0530

    Create multiplication EX-3.R

---
+++ multiplication EX-3.R
@@ -0,0 +1,6 @@
+@@ -0,0 +1,5 @@
+@@ -0,0 +1,5 @@
+num1=as.integer(readline(prompt = "Enter the num 1:")) 
+num2 =as.integer(readline(prompt = "Enter a number2:")) 
+mul=num1*num2 
+print((paste("Multiplication:",mul)))
